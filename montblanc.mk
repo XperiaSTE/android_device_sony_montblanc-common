a64917faaf2663e796dcf8da0e84847ce56b6d15
XianGxin <xiangxin19960319@gmail.com>
2014-06-16 15:53:12 +0800

    wifi direct(test 1)

--- montblanc.mk
+++ montblanc.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
         packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
+# Wifi direct (test)
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+
 # Configuration files
 PRODUCT_COPY_FILES += \
         device/sony/montblanc-common/config/media_codecs.xml:system/etc/media_codecs.xml \
@@ -47,15 +51,16 @@ PRODUCT_COPY_FILES += \
         device/sony/montblanc-common/config/10wireless:system/etc/init.d/10wireless \
         device/sony/montblanc-common/config/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	device/sony/montblanc-common/config/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
-        device/sony/montblanc-common/config/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-        device/sony/montblanc-common/config/regulatory.rules:system/etc/udev/rules.d/85-regulatory.rules
+        device/sony/montblanc-common/config/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf
 
+# Edit crda for st-ericsson
 # CRDA
 PRODUCT_PACKAGES += \
        crda \
        regdbdump \
        regulatory.bin \
-       linville.key.pub.pem
+       linville.key.pub.pem \
+       85-regulatory.rules
 
 # KitKat Launcher
 PRODUCT_PACKAGES += Launcher3        
