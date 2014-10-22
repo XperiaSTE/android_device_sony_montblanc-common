bedbfbc2615a05b8b32382e594484627312191d9
Agontuk <rifat662@gmail.com>
2014-10-22 18:18:57 +0600

    Add missing input device configuration files

--- montblanc.mk
+++ montblanc.mk
@@ -153,5 +153,10 @@ PRODUCT_COPY_FILES += \
         device/sony/montblanc-common/config/gps.conf:system/etc/gps.conf \
 	device/sony/montblanc-common/config/cacert.txt:system/etc/suplcert/cacert.txt
 
+# Copy input device configurations
+PRODUCT_COPY_FILES += \
+        device/sony/montblanc-common/config/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc \
+        device/sony/montblanc-common/config/synaptics_rmi4_i2c.idc:system/usr/idc/synaptics_rmi4_i2c.idc
+
 # Garbage Collector type
 PRODUCT_TAGS += dalvik.gc.type-precise
