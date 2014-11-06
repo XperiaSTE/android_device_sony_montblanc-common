89f65f2b0fe9521120d6e5e5bebdea84924c7395
Agontuk <rifat662@gmail.com>
2014-11-06 22:38:38 +0600

    Use default init.environ.rc
    We used separate init.environ.rc to export STE-OMX library path.
    Since LD_LIBRARY_PATH is removed in Lollipop, we can use the default
    one instead.
    
    TODO: Find another way to export omx library path ?

--- montblanc.mk
+++ montblanc.mk
@@ -136,7 +136,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Custom init scripts
 PRODUCT_COPY_FILES += \
-        device/sony/montblanc-common/config/init.environ.rc:root/init.environ.rc \
         device/sony/montblanc-common/config/init.st-ericsson.rc:root/init.st-ericsson.rc \
         device/sony/montblanc-common/config/ueventd.st-ericsson.rc:root/ueventd.st-ericsson.rc \
         external/koush/Superuser/init.superuser.rc:root/init.superuser.rc
