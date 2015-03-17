5068cb790083fb30095553fd635fa5edebdbee4d
Andrea Mennillo <handymenny@outlook.com>
2015-03-17 20:28:54 +0100

    Add required sepolicy files for montblanc
    This change also adapt init.st-ericsson.rc and 01stesetup to be
    compatible with SELinux.
    Note: bluetoothd.te and bluetoothd_snoop.te came from bluez 5.29
    
    Change-Id: I1073047e58d0d044a85177712b3cd36495bdbaa6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,3 +119,29 @@ TW_NO_CPU_TEMP := true
 
 # System proprieties
 TARGET_SYSTEM_PROP := device/sony/montblanc-common/system.prop
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/sony/montblanc-common/sepolicy
+BOARD_SEPOLICY_UNION += \
+       admsrv.te \
+       atd.te \
+       bluetoothd.te \
+       bluetoothd_snoop.te \
+       cn_server.te \
+       conn_init.te \
+       copsd.te \
+       device.te \
+       execmem.te \
+       gpsd.te \
+       hci_attach.te \
+       init.te \
+       iw.te \
+       modem-helpers.te \
+       psccd.te \
+       rild.te \
+       simd.te \
+       sms_server.te \
+       suntrold.te \
+       tad.te \
+       watchdogd.te \
+       wpa.te
