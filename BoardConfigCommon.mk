55a36900f475042883c39f7ec80bf2c147904ff0
Agontuk <rifat662@gmail.com>
2015-04-06 20:39:30 +0600

    Revert "montblanc-common: use dlmalloc instead of jemalloc"
    causes random heap corruption in camera
    
    This reverts commit 4e28d9aacbe52dbd3f4659bb22e1eca612c01a1a.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,7 +22,6 @@ ARCH_ARM_HAVE_NEON := true
 # Bionic flags
 BOARD_USES_LEGACY_MMAP := true
 TARGET_NEEDS_BIONIC_MD5 := true
-MALLOC_IMPL := dlmalloc
 
 # RIL implementation
 BOARD_RIL_CLASS := ../../../device/sony/montblanc-common/telephony-common/
