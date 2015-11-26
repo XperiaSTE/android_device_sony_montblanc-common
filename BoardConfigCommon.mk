5efdaac72f0b297dc6d71f91c5d2aa462b2f87b5
Iftekhar Rifat <rifat662@gmail.com>
2015-11-26 14:13:00 +0600

    Remove exFAT from build
    Change-Id: I871556faa479cd9a510bba6bbd6a52bb871702a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,6 +122,7 @@ TARGET_SYSTEMIMAGE_USE_SQUISHER := true
 ifeq ($(HOST_OS),linux)
 TARGET_USERIMAGES_USE_F2FS := true
 endif
+WITH_EXFAT := false
 
 # Disable secure discard
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
